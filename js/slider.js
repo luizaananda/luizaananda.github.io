d91b13cb9c665c3513157ad3549ccbd8183fee5a
luizaananda <luizaanandamac@gmail.com>
2020-04-08 12:50:35 -0300

    Seleçao de comentarios

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,29 @@
-var slideInterval = 5000;
-
 function getComments() {
     return document.getElementById('slider').getElementsByClassName('comment');
 }
 
-function moveForward() {
-    var pointer = 0;
-    var comments = getComments();
-    for (var i = 0; i < comments.length; i++) {
-        if (comments[i].classList.contains('visible')) {
-            comments[i].classList.remove('visible');
-            comments[i].classList.add('hidden');
-            pointer = i;
-        }
-    }
-    if (++pointer == comments.length) {
-        pointer = 0;
-    }
-    comments[pointer].classList.add('visible');
-    setTimeout(moveForward, slideInterval);
+function getDots() {
+    return document.getElementById('slider').getElementsByClassName('dot');
+
 }
 
-function startPlayback() {
-    setTimeout(moveForward, slideInterval);
+var dots = getDots();
+var comments = getComments();
+
+for (var i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", changeComments);
+
 }
 
-
-startPlayback();
+function changeComments(event) {
+    const children = Array.prototype.slice.call(dots);
+    for (var i = 0; i < dots.length; i++) {
+        comments[i].classList.remove('visible');
+        comments[i].classList.add('hidden');
+        dots[i].classList.remove('selected');
+        dots[i].classList.add('hidden');
+    }
+    const index = children.indexOf(event.target);
+    event.target.classList.add('selected');
+    comments[index].classList.add('visible');
+}
